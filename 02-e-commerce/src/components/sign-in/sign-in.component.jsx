3038d47e2963fe45a9938214cc97cbf777be12b8
Daniel Florez <danielflorez111@gmail.com>
2020-05-19 21:40:52 -0500

    Adding FormInput and CustomButton components

--- 02-e-commerce/src/components/sign-in/sign-in.component.jsx
+++ 02-e-commerce/src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import './sign-in.styles.scss';
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 class SignIn extends React.Component {
 
@@ -34,26 +36,25 @@ class SignIn extends React.Component {
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
                 <form onSubmit={this.handleSubmit}>
-                    <input
+                    <FormInput
                         name="email"
                         type="email"
+                        label="Email"
                         value={this.state.email}
-                        onChange={this.handleChange}
+                        handleChange={this.handleChange}
                         required
                     />
-                    <label>Email</label>
-                    <input
+                    <FormInput
                         name="password"
                         type="password"
+                        label="Password"
                         value={this.state.password}
-                        onChange={this.handleChange}
+                        handleChange={this.handleChange}
                         required
                     />
-                    <label>Password</label>
-                    <input
-                        type="submit"
-                        value="Submit Form"
-                    />
+                    <CustomButton type="submit">
+                        Sign In
+                    </CustomButton>
                 </form>
             </div>
         );
