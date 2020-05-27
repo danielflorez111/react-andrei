d237fd29be58b1badae97749788037bdc8808075
Daniel Florez <danielflorez111@gmail.com>
2020-05-26 22:12:30 -0500

    Adding reselect to cart reducer

--- 02-e-commerce/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ 02-e-commerce/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import './cart-dropdown.styles.scss';
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 
-const CartDropdown = () => {
+const CartDropdown = ({ cartItems }) => {
     return (
         <div className="cart-dropdown">
             <div className="cart-items">
-                <CustomButton>GO TO CHECKOUT</CustomButton>
+                {
+                    cartItems.map(item =>
+                        <CartItem
+                            key={item.id}
+                            item={item}
+                        />
+                    )
+                }
             </div>
+            <CustomButton>GO TO CHECKOUT</CustomButton>
         </div>
     );
 };
 
-export default CartDropdown;
+const mapStateToProps = (state) => ({
+    cartItems: selectCartItems(state)
+});
+
+export default connect(mapStateToProps)(CartDropdown);
