daaf1ce8ba8e2760ff0232ee92841b7277a25e22
Daniel Florez <danielflorez111@gmail.com>
2020-07-05 17:48:04 -0500

    Adding cart sagas and refactor shop sagas

--- 02-e-commerce/src/redux/shop/shop.sagas.js
+++ 02-e-commerce/src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import { takeLatest, call, put } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 
 import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 import { ShopActionTypes } from './shop.types';
@@ -17,8 +17,11 @@ export function* fetchCollectionsStartAsync() {
 }
 
 export function* fetchCollectionsStart() {
-    yield takeLatest(
-        ShopActionTypes.FETCH_COLLECTIONS_START,
-        fetchCollectionsStartAsync
-    );
+    yield takeLatest(ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollectionsStartAsync);
+}
+
+export function* shopSagas() {
+    yield all([
+        call(fetchCollectionsStart)
+    ]);
 }
