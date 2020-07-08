590544bf6d4c1a28fb4b4db1f5b9cce40aeb431f
Daniel Florez <danielflorez111@gmail.com>
2020-07-07 23:04:42 -0500

    Integrating Stripe payments

--- 02-e-commerce/client/src/components/stripe-button/stripe-button.component.jsx
+++ 02-e-commerce/client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_pau8xqLlw5Q9euvZdLv5kNPK00lfqiSw3C';
 
     const onToken = token => {
-        console.log(token);
-        alert('Payment Successful');
+        axios({
+            url: 'payment',
+            method: 'POST',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert('Payment successful');
+        }).catch(error => {
+            console.log('Payment error ', JSON.parse(error));
+            alert('There was an issue with your payment, Please make sure you use the provided credit card');
+        });
     }
 
     return (
