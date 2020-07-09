8cb9feb15468fee7347a9e78f1617e2ea8b85eaa
Daniel Florez <danielflorez111@gmail.com>
2020-07-09 17:41:04 -0500

    Context API part 1

---
+++ 03-react-context-lesson/src/firebase/firebase.utils.js
@@ -0,0 +1,50 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyAz6MQQy65bfJVsBI1W2RjhPvSo6v6_riE",
+  authDomain: "e-commerce-8817e.firebaseapp.com",
+  databaseURL: "https://e-commerce-8817e.firebaseio.com",
+  projectId: "e-commerce-8817e",
+  storageBucket: "e-commerce-8817e.appspot.com",
+  messagingSenderId: "790229011732",
+  appId: "1:790229011732:web:00dd2e2dec632a413c0601",
+  measurementId: "G-45HCXSM14V"
+};
+
+firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
